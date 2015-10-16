0a87a7681dccb38254e2f433c60bac3c2ec451ca
d-smith <doug.smith.mail@gmail.com>
2015-10-16 06:19:48 -0600

    Added functional composition example

---
+++ README.md
@@ -0,0 +1,2 @@
+Code exercises and samples while reading Functional Programming in Java
+by Pierre-Yves Saumont
--- src/main/java/funciface/FunctionalInterface.java
+++ src/main/java/funciface/FunctionalInterface.java
@@ -2,6 +2,16 @@ package funciface;
 
 
 public class FunctionalInterface {
+
+    public static Function compose(final Function f1, final Function f2) {
+        return new Function() {
+            @Override
+            public int apply(int arg) {
+                return f1.apply(f2.apply(arg));
+            }
+        };
+    }
+
     public static void main(String[] args) {
         Function triple = new Function() {
             @Override
@@ -10,6 +20,16 @@ public class FunctionalInterface {
             }
         };
 
+        Function square = new Function() {
+            @Override
+            public int apply(int arg) { return arg * arg;}
+        };
+
         System.out.println("Triple 3 is " + triple.apply(3));
+        System.out.println("Square 3 is " + square.apply(3));
+
+        Function tripleThenSquare = compose(square, triple);
+
+        System.out.println("triple then square 2 is " + tripleThenSquare.apply(2));
     }
 }
