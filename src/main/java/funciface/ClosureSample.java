bebb83ee2a6ca6bf20de819196fb1f451c882174
d-smith <doug.smith.mail@gmail.com>
2015-10-21 08:38:48 -0600

    Added curried version of add tax

--- src/main/java/funciface/ClosureSample.java
+++ src/main/java/funciface/ClosureSample.java
@@ -9,5 +9,9 @@ public class ClosureSample {
         Function<Double,Double> addTax = x -> x + computeTax.apply(x);
 
         System.out.println(addTax.apply(10.0));
+
+        Function<Double, Function<Double,Double>> curriedAddTax = x -> y -> y + y * x;
+
+        System.out.println(curriedAddTax.apply(taxRate).apply(10.0));
     }
 }
