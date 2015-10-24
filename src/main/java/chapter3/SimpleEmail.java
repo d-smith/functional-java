9867b1642fd2ff16f712719cba8cfdd0b3a61c8f
d-smith <doug.smith.mail@gmail.com>
2015-10-24 07:50:26 -0600

    introduced Result to email sample to handle result of computation

--- src/main/java/chapter3/SimpleEmail.java
+++ src/main/java/chapter3/SimpleEmail.java
@@ -8,18 +8,26 @@ import java.util.regex.Pattern;
 
 //1st version- imperative, mixed processing with effects
 //2nd - separate computation from effects
+//3rd - introduce something to handle the result of the computation
 public class SimpleEmail {
     final static Pattern emailPattern = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
-    final static Function<String, Boolean> emailChecker = s -> emailPattern.matcher(s).matches();
-
-    static void testMail(String email) {
-        if(emailChecker.apply(email)) {
-            sendVerificationMail(email);
-        } else {
-            logError("email " + email + " is invalid.");
+    final static Function<String, Result> emailChecker = s -> {
+        if (s == null) {
+            return new Result.Failure("email must not be null");
         }
-    }
+
+        if (s.length() == 0) {
+            return new Result.Failure("email must not be empty");
+        }
+
+        if (emailPattern.matcher(s).matches()) {
+            return new Result.Success();
+        } else {
+            return new Result.Failure("email " + s + " is invalid.");
+        }
+    };
+
 
     static void sendVerificationMail(String email) {
         System.out.println("Verification mail send to " + email);
@@ -29,8 +37,18 @@ public class SimpleEmail {
         System.err.println("Error message logged: " + s);
     }
 
+    static void validate(String s) {
+        Result result = emailChecker.apply(s);
+        if (result instanceof Result.Success) {
+            sendVerificationMail(s);
+        } else {
+            logError(((Result.Failure) result).getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        testMail("foo@bar.com");
-        testMail("huh?");
+        validate("foo@bar.com");
+        validate(null);
+        validate("");
     }
 }
