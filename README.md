bcbdfbb859da8a1418d9e7c93c696e3495666906
d-smith <doug.smith.mail@gmail.com>
2015-10-26 07:55:02 -0600

    Eliminated if/then/else in email checker via introduction of Case class

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-Code exercises and samples while reading Functional Programming in Java
+Code exercises and samples while reading [Functional Programming in Java](https://www.manning.com/books/functional-programming-in-java)
 by Pierre-Yves Saumont
+
+The code in the com.fpinjava.common package is taken from from the [github repo
+associated with the book](https://github.com/fpinjava/fpinjava)
+
--- src/main/java/chapter3/SimpleEmail.java
+++ src/main/java/chapter3/SimpleEmail.java
@@ -1,9 +1,15 @@
 package chapter3;
 
 
-import funciface.Function;
+import com.fpinjava.common.Function;
 
 import java.util.regex.Pattern;
+
+import static chapter3.Case.mcase;
+import static chapter3.Case.match;
+import static chapter3.Result.failure;
+import static chapter3.Result.success;
+
 
 
 //1st version- imperative, mixed processing with effects
@@ -11,24 +17,18 @@ import java.util.regex.Pattern;
 //3rd - introduce something to handle the result of the computation
 //4th - update validate to return an executable value instead of validate just having an effect
 //5th - Eliminate instanceof test, allow configuring what to do based on validation result
+//6th - Eliminate if then else control structure using case and match
 public class SimpleEmail {
     static Pattern emailPattern = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
-    static Function<String, Result<String>> emailChecker = s -> {
-        if (s == null) {
-            return Result.failure("email must not be null");
-        }
-
-        if (s.length() == 0) {
-            return Result.failure("email must not be empty");
-        }
-
-        if (emailPattern.matcher(s).matches()) {
-            return Result.success(s);
-        } else {
-            return Result.failure("email " + s + " is invalid.");
-        }
-    };
+    static Function<String, Result<String>> emailChecker = s ->
+        match(
+                mcase(() -> success(s)),
+                mcase(() -> s == null, () -> failure("email must not be null")),
+                mcase(() -> s.length() == 0, () -> failure("email must not be empty")),
+                mcase(() -> !emailPattern.matcher(s).matches(), () -> failure("email " + s + " is invalid."))
+        );
+    
 
 
     static Effect<String> success = s -> System.out.println("Verification mail send to " + s);
