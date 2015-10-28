08a481718513220dd084461517a0d5d19dc406a2
d-smith <doug.smith.mail@gmail.com>
2015-10-28 10:58:47 -0600

    Added list creation utility methods

--- src/main/java/chapter3/CollectionUtilities.java
+++ src/main/java/chapter3/CollectionUtilities.java
@@ -4,6 +4,7 @@ package chapter3;
 import com.fpinjava.common.Function;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CollectionUtilities {
@@ -13,5 +14,22 @@ public class CollectionUtilities {
             newList.add(f.apply(value));
         }
         return newList;
+    }
+
+    public static <T> List<T> list() {
+        return Arrays.asList();
+    }
+
+    public static <T> List<T> list(T t) {
+        return Arrays.asList(t);
+    }
+
+    public static <T> List<T> list(List<T> l) {
+        return new ArrayList<>(l);
+    }
+
+    @SafeVarargs
+    public static <T> List<T> list(T... t) {
+        return Arrays.asList(t);
     }
 }
