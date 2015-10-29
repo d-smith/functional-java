5afc1963391521e68ec66a34dacf2df5a40b2ad9
d-smith <doug.smith.mail@gmail.com>
2015-10-29 08:18:56 -0600

    Added head and tail to collection utilities

--- src/main/java/chapter3/CollectionUtilities.java
+++ src/main/java/chapter3/CollectionUtilities.java
@@ -8,13 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CollectionUtilities {
-   public static  <T,U> List<U> map(List<T> l, Function<T,U> f) {
-        List<U> newList = new ArrayList<>();
-        for(T value: l) {
-            newList.add(f.apply(value));
-        }
-        return newList;
-    }
+
 
     public static <T> List<T> list() {
         return Arrays.asList();
@@ -31,5 +25,43 @@ public class CollectionUtilities {
     @SafeVarargs
     public static <T> List<T> list(T... t) {
         return Arrays.asList(t);
+    }
+
+    public static  <T,U> List<U> map(List<T> l, Function<T,U> f) {
+        List<U> newList = new ArrayList<>();
+        for(T value: l) {
+            newList.add(f.apply(value));
+        }
+        return newList;
+    }
+
+    public static <T> T head(List<T> l) {
+        if(l == null) {
+            throw new IllegalStateException("head of null list");
+        }
+
+        if(l.size() == 0) {
+            throw new IllegalStateException("head of empty list");
+        }
+
+        return l.get(0);
+    }
+
+    public static <T> List<T> copy(List<T> l) {
+        return new ArrayList<>(l);
+    }
+
+    public static <T> List<T> tail(List<T> l) {
+        if(l == null) {
+            throw new IllegalStateException("tail of null list");
+        }
+
+        if(l.size() == 0) {
+            throw new IllegalStateException("tail of empty list");
+        }
+
+        List<T> workList = copy(l);
+        workList.remove(0);
+        return workList;
     }
 }
