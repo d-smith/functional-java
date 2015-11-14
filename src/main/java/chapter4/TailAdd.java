ff9045fdee6b8a98d599ce191c8002597fdebe64
d-smith <doug.smith.mail@gmail.com>
2015-11-14 09:59:33 -0700

    Added self contained add

--- src/main/java/chapter4/TailAdd.java
+++ src/main/java/chapter4/TailAdd.java
@@ -1,6 +1,8 @@
 package chapter4;
 
 
+import com.fpinjava.common.Function;
+
 import static chapter4.TailCall.ret;
 import static chapter4.TailCall.sus;
 
@@ -15,7 +17,19 @@ public class TailAdd {
                 : sus(() -> addRec(x + 1, y - 1));
     }
 
+    static Function<Integer,Function<Integer,Integer>> selfContainedAdd = x -> y ->
+    {
+        class AddHelper {
+            Function<Integer,Function<Integer,TailCall<Integer>>> addHelper = a -> b ->
+                    b == 0 ? ret(a)
+                            : sus(()->this.addHelper.apply(a + 1).apply(b - 1));
+        }
+
+        return new AddHelper().addHelper.apply(x).apply(y).eval();
+    };
+
     public static void main(String... args) {
         System.out.println(add(3, 100000000));
+        System.out.println(selfContainedAdd.apply(3).apply(100000000));
     }
 }
