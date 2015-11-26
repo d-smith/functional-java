1926baac3dbd67cc529e5fecdfadd86eb4865767
d-smith <doug.smith.mail@gmail.com>
2015-11-26 06:38:21 -0800

    Added stack safe function composition

--- src/main/java/chapter4/Composition.java
+++ src/main/java/chapter4/Composition.java
@@ -5,8 +5,11 @@ import com.fpinjava.common.Function;
 
 import java.util.List;
 
+import static chapter3.CollectionUtilities.list;
 import static chapter3.CollectionUtilities.map;
+import static chapter3.CollectionUtilities.reverse;
 import static chapter3.Range.range;
+import static chapter4.StackSafeCollectionUtils.foldLeft;
 import static chapter4.StackSafeCollectionUtils.foldRight;
 import static com.fpinjava.common.Function.identity;
 
@@ -15,10 +18,19 @@ public class Composition {
         return foldRight(list, identity(), x -> y -> x.compose(y));
     }
 
+    static <T> Function<T,T> composeAllViaFoldLeft(List<Function<T,T>> list) {
+        return x -> foldLeft(reverse(list), x, a -> b -> b.apply(a));
+    }
+
     public static void main(String... args) {
         Function<Integer,Integer> add = y -> y + 1;
         System.out.println(
                 composeAll(map(range(0,500),x -> add)).apply(0)
         );
+
+        Function<String,String> f1 = x -> "(a" + x + ")";
+        Function<String,String> f2 = x -> "{b" + x + "}";
+        Function<String,String> f3 = x -> "[c" + x + "]";
+        System.out.println(composeAllViaFoldLeft(list(f1,f2,f3)).apply("x"));
     }
 }
