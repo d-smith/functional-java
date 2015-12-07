cd0ad260aea443943172a474e6e1b6a2624325d9
d-smith <doug.smith.mail@gmail.com>
2015-12-07 05:38:35 -0800

    Memoization of curried function

--- src/main/java/chapter4/Memoizer.java
+++ src/main/java/chapter4/Memoizer.java
@@ -2,11 +2,9 @@ package chapter4;
 
 
 import com.fpinjava.common.Function;
-import com.sun.org.apache.xpath.internal.compiler.FunctionTable;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class Memoizer<T,U> {
     private final Map<T,U> cache = new ConcurrentHashMap<>();
@@ -35,7 +33,11 @@ public class Memoizer<T,U> {
 
     private static Function<Integer,Integer> g = Memoizer.memoize(f);
 
-    public static void main(String... args) {
+    private static Function<Integer,Function<Integer, Function<Integer,Integer>>> f3m =
+            Memoizer.memoize(x -> Memoizer.memoize(y -> Memoizer.memoize(z ->
+                longCalculation(x) + longCalculation(y) + longCalculation(z))));
+
+    public static void memoizationExample1() {
         long startTime = System.currentTimeMillis();
         Integer result1 = g.apply(1);
         long time1 = System.currentTimeMillis() - startTime;
@@ -46,5 +48,24 @@ public class Memoizer<T,U> {
         System.out.println(result2);
         System.out.println(time1);
         System.out.println(time2);
+    }
+
+    public static void memoizationExample2() {
+        long startTime = System.currentTimeMillis();
+        Integer result1 = f3m.apply(2).apply(3).apply(4);
+        long time1 = System.currentTimeMillis() - startTime;
+        startTime = System.currentTimeMillis();
+        Integer result2 = f3m.apply(2).apply(3).apply(4);
+        long time2 = System.currentTimeMillis() - startTime;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(time1);
+        System.out.println(time2);
+
+    }
+
+    public static void main(String... args) {
+        memoizationExample1();
+        memoizationExample2();
     }
 }
