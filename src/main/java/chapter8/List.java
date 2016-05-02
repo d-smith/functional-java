4b5672dbff1426d2db2a80557f04fef74a30c265
d-smith <doug.smith@fmr.com>
2016-05-02 08:26:26 -0700

    Start of chapter 8 list updates

---
+++ src/main/java/chapter8/List.java
@@ -0,0 +1,263 @@
+package chapter8;
+
+
+import chapter4.TailCall;
+import com.fpinjava.common.Function;
+
+import static chapter4.TailCall.ret;
+import static chapter4.TailCall.sus;
+
+public abstract class List<A> {
+    public abstract A head();
+    public abstract List<A> tail();
+    public abstract boolean isEmpty();
+    public abstract List<A> cons(A a);
+    public abstract List<A> setHead(A a);
+    public abstract List<A> drop(int n);
+    public abstract List<A> dropWhile(Function<A, Boolean> f);
+    public abstract List<A> init();
+    public abstract List<A> reverse();
+    public abstract int length();
+    public abstract <B> B foldLeft(B identity, Function<B, Function<A, B>> f);
+    public abstract <B> B foldRight(B identity, Function<A, Function<B,B>>  f);
+    public abstract <B> List<B> map(Function<A,B> f);
+    public abstract List<A> filter(Function<A,Boolean> f);
+    public abstract List<A> filterViaFlatmap(Function<A,Boolean> f);
+    public abstract <B> List<B> flatMap(Function<A, List<B>>f);
+
+
+    @SuppressWarnings("rawtypes")
+    public static final List NIL = new Nil();
+
+    private List() {}
+
+    private static class Nil<A> extends List<A> {
+        private Nil() {}
+
+        public A head() {
+            throw new IllegalStateException("head called on Nil list");
+        }
+
+        public List<A> tail() {
+            throw new IllegalStateException("tail called on Nil list");
+        }
+
+        public boolean isEmpty() {
+            return true;
+        }
+
+        public List<A> cons(A a) {
+            return new Cons<>(a, this);
+        }
+
+        public List<A> setHead(A a) {
+            throw new IllegalStateException("setHead called on empty list");
+        }
+
+        public List<A> drop(int n) { return this; }
+
+        public List<A> dropWhile(Function<A, Boolean> f) { return this; }
+
+        public List<A> init() { return this; }
+
+        public List<A> reverse() { return this; }
+
+        public int length() { return 0; }
+
+        @Override
+        public <B> B foldLeft(B identity, Function<B, Function<A, B>> f) {
+            return identity;
+        }
+
+        @Override
+        public <B> B foldRight(B identity, Function<A, Function<B, B>> f) { return identity; }
+
+        @Override
+        public <B> List<B> map(Function<A, B> f) {return foldRight(list(), h -> t -> new Cons<>(f.apply(h),t));}
+
+        @Override
+        public List<A> filter(Function<A, Boolean> f) {
+            return foldRight(list(), h -> t -> f.apply(h) ? new Cons<>(h,t) : t);
+        }
+
+        @Override
+        public List<A> filterViaFlatmap(Function<A, Boolean> f) {
+            return flatMap(a -> f.apply(a) ? List.list(a) : List.list());
+        }
+
+        @Override
+        public <B> List<B> flatMap(Function<A, List<B>> f) {
+            return foldRight(list(),h -> t -> concat(f.apply(h),t));
+        }
+    }
+
+    private static class Cons<A> extends List<A> {
+        private final A head;
+        private final List<A> tail;
+
+        private Cons(A head, List<A> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        public A head() {
+            return head;
+        }
+
+        public List<A> tail() {
+            return tail;
+        }
+
+        public boolean isEmpty() {
+            return false;
+        }
+
+        public List<A> cons(A a) {
+            return new Cons<>(a,this);
+        }
+
+        public List<A> setHead(A a) {
+            return new Cons(a,tail());
+        }
+
+        public List<A> drop(int n) {
+            return n <= 0
+                    ? this
+                    : drop_(this,n).eval();
+        }
+
+        private TailCall<List<A>> drop_(List<A> list, int n) {
+            return n <= 0 || list.isEmpty()
+                    ? ret(list)
+                    : sus(() -> drop_(list.tail(), n - 1));
+        }
+
+        public List<A> dropWhile(Function<A, Boolean> f) {
+            return dropWhile_(this,f).eval();
+        }
+
+        private TailCall<List<A>> dropWhile_(List<A> list, Function<A, Boolean> f) {
+            return !list.isEmpty() && f.apply(list.head())
+                    ? sus(() -> dropWhile_(list.tail(), f))
+                    : ret(list);
+        }
+
+        public List<A> reverse() {
+            return foldLeft(list(), x -> x::cons);
+        }
+
+
+
+        public List<A> init() {
+            return reverse().tail().reverse();
+        }
+
+        public int length() {
+           //Internal compiler error -->  return this.foldLeft(0, x -> y -> x + 1);
+           //Internal compiler error --> return foldRight(0, x -> y -> y + 1);
+            return length_(this, 0).eval();
+        }
+
+        private TailCall<Integer> length_(List<A> list, int acc) {
+            return list.isEmpty()
+                    ? ret(acc)
+                    : sus(() -> length_(list.tail(), acc + 1));
+        }
+
+        @Override
+        public <B> B foldLeft(B identity, Function<B, Function<A, B>> f) {
+            return foldLeft_(identity, this, f).eval();
+        }
+
+        public <B> TailCall<B> foldLeft_(B acc, List<A> list, Function<B, Function<A, B>> f) {
+            return list.isEmpty()
+                    ? ret(acc)
+                    : sus(() -> foldLeft_(f.apply(acc).apply(list.head()), list.tail(),f));
+        }
+
+        @Override
+        public <B> B foldRight(B identity, Function<A, Function<B, B>> f) {
+            //return foldRight_(identity, this, identity, f).eval();
+            return reverse().foldLeft(identity, x -> y -> f.apply(y).apply(x));
+        }
+
+        private <B> TailCall<B> foldRight_(B acc, List<A> list, B identity, Function<A,Function<B,B>> f) {
+            return list.isEmpty()
+                        ? ret(acc)
+                        : sus(() -> foldRight_(f.apply(list.head()).apply(acc), list.tail(),identity,f));
+        }
+
+        @Override
+        public <B> List<B> map(Function<A, B> f) {return foldRight(list(), h -> t -> new Cons<>(f.apply(h),t));}
+
+        @Override
+        public List<A> filter(Function<A, Boolean> f) {
+            return foldRight(list(), h -> t -> f.apply(h) ? new Cons<>(h,t) : t);
+        }
+
+        @Override
+        public List<A> filterViaFlatmap(Function<A, Boolean> f) {
+            return flatMap(a -> f.apply(a) ? List.list(a) : List.list());
+        }
+
+        @Override
+        public <B> List<B> flatMap(Function<A, List<B>> f) {
+            return foldRight(list(),h -> t -> concat(f.apply(h),t));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <A> List<A> list() {
+        return NIL;
+    }
+
+    @SafeVarargs
+    public static <A> List<A> list(A... a) {
+        List<A> n = list();
+        for(int i = a.length - 1; i >= 0; i--) {
+            n = new Cons<>(a[i], n);
+        }
+
+        return n;
+    }
+
+    public String toString() {
+        return String.format("[%sNIL]",
+                toString(new StringBuilder(), this).eval());
+    }
+
+    public static <A, B> B foldRight(List<A> list, B n, Function<A, Function<B, B>> f ) {
+        return list.foldRight(n, f);
+    }
+
+    public static <A> List<A> concat(List<A> list1, List<A> list2) {
+        return foldRight(list1, list2, x -> y -> new Cons<>(x, y));
+    }
+
+    private TailCall<StringBuilder> toString(StringBuilder acc, List<A> list) {
+        return list.isEmpty() ?
+                ret(acc)
+                : sus(()->toString(acc.append(list.head()).append(","),list.tail()));
+    }
+
+    public static <A> List<A> flatten(List<List<A>> list) {
+        return list.flatMap(x -> x);
+    }
+
+    public static Integer sum(List<Integer> list) {
+        return list.foldLeft(0,x -> y -> x + y);
+    }
+
+    public static Double product(List<Double> list) {
+        return list.foldLeft(1.0, x -> y -> x * y);
+    }
+
+    public static List<Integer> triple(List<Integer> list) {
+        return foldRight(list, list(), x -> y -> y.cons(x * 3));
+    }
+
+    public static List<String> doubleToString(List<Double> list) {
+        return foldRight(list, list(), x -> y -> y.cons(x.toString()));
+    }
+
+}
---
+++ src/main/java/chapter8/ListSample.java
@@ -0,0 +1,56 @@
+package chapter8;
+
+
+import com.fpinjava.common.Function;
+
+import static chapter8.List.list;
+
+public class ListSample {
+
+    public static void main(String... args) {
+        List<Integer> ex1 = list();
+        List<Integer> ex2 = list(1);
+        List<Integer> ex3 = list(1, 2);
+
+        System.out.println(ex3);
+
+        List<Integer> ex4 = ex2.cons(0);
+        System.out.println(ex4);
+
+        System.out.println("set head on ex4 to -1");
+        ex4 = ex4.setHead(-1);
+        System.out.println(ex4);
+
+        System.out.println("\nA list:");
+        List<Integer> bl = list(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+        System.out.println(bl);
+        System.out.println(bl.drop(1));
+        System.out.println(bl.drop(5));
+        System.out.println(bl.drop(100));
+
+        Function<Integer,Boolean> lessThanTen = (i) -> i < 10;
+        System.out.println(bl.dropWhile(lessThanTen));
+
+        System.out.println(List.concat(bl, bl));
+        System.out.println(bl.init());
+        System.out.println(List.sum(list(1,2,3,4)));
+        System.out.println(List.product(list(1.0,2.0,3.0,4.0)));
+        System.out.println("length of big list: " + bl.length());
+
+        System.out.println(List.triple(bl));
+
+        List<Double> dl = list(1.0,2.0, 3.0);
+        System.out.println(List.doubleToString(dl));
+
+        System.out.println(bl.map(x -> x*3));
+
+        System.out.println(bl.filter(x -> x % 2 == 0));
+        System.out.println(bl.filterViaFlatmap(x -> x % 2 == 0));
+
+        System.out.println(list(1,2,3).flatMap(x -> list(x,-x)));
+
+        System.out.println(list(1,2,3,4,5).reverse());
+
+        System.out.println(List.flatten(list(list(1,2,3),list(4,5,6))));
+    }
+}
